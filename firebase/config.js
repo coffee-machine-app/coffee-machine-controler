875755f4a486c9905a78eb59e115d4286f2b2081
garnier5 <garnier21axel@gmail.com>
2020-10-12 14:08:28 +0200

    adding firebase config

---
+++ firebase/config.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+require("firebase/database");
+
+//Get the config given by firebase
+var firebaseConfig = {
+
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export { firebase };
